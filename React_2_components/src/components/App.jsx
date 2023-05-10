413b53dab2626d4c0dfe9aa52ad6c982e5432700
rchaubey <rchaubey@micron.com>
2023-05-10 23:42:32 +0530

    commented for some details

--- React_2_components/src/components/App.jsx
+++ React_2_components/src/components/App.jsx
@@ -33,12 +33,12 @@ function App() {
 export default App;
 
 
-// This is the way to pass parameters to a react component
-/* <Cards
-name={contacts[2].name}
-img={contacts[2].imgURL}
-tel={contacts[2].phone}
-email={contacts[2].email}
-/> */
+{/* // This is the way to pass parameters to a react component */}
+{/* /* <Cards */}
+{/* name={contacts[2].name} */}
+{/* img={contacts[2].imgURL} */}
+{/* tel={contacts[2].phone} */}
+{/* email={contacts[2].email} */}
+{/* /> */ */}
 
 {/* To remove the above repeatation of cards components 3 times we can use map function */}
